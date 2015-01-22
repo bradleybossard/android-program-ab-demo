a6e67d0bd514828441c0635c165c2a0f070e02cb
Bradley Bossard <bradleybossard@gmail.com>
2015-01-21 16:54:46 -0800

    Adding Ab.jar library, adding some basic Activity code.  It compiles and runs, but currently does nothing.

--- app/src/main/java/com/bradleybossard/androidprogramabdemo/MainActivity.java
+++ app/src/main/java/com/bradleybossard/androidprogramabdemo/MainActivity.java
@@ -1,19 +1,66 @@
 package com.bradleybossard.androidprogramabdemo;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+
+//import android.R.string;
+//import android.provider.Settings.System;
+//import android.util.Log;
 
 
-public class MainActivity extends ActionBarActivity {
+/*
+Hello.  I was curious, has there been any progress on this, i.e. getting Program AB to run on Android.
+
+I was kind of looking over this thread and another related one here
+
+https://www.chatbots.org/ai_zone/viewthread/1778/
+
+I have created a new Android Studio project and copied over the AB.jar library from the Project AB, but I realize now when diving a little deeper, that when you pass a path to Bot constructor, like so
+
+String path = "/bots";
+Bot bot = new Bot(botname, path);
+
+
+http://stackoverflow.com/questions/26546204/aiml-file-does-not-exist-why-my-android-project-cannot-connect-to-aiml-files-in/26973015#26973015
+*/
+
+
+public class MainActivity extends Activity {
+
+    String botname = "alice2";
+
+    // TODO(bossard) : Ok, so when you create a bot, it needs a file structure like this
+    //
+    //  Directory	Contents
+/*
+    c:/example/bots/mybot/aiml	AIML Files
+    c:/example/bots/mybot/aimlif	AIMLIF format files
+    c:/example/bots/mybot/config	Bot configuration Files
+    c:/example/bots/mybot/sets	AIML Sets
+    c:/example/bots/mybot/maps	AIML Maps
+*/
+  //
+    // TODO(bbossard) : Possible solution to overcoming the file problem path, only sync .zip file, then unzip it locally and use the files.
+    //http://stackoverflow.com/questions/26546204/aiml-file-does-not-exist-why-my-android-project-cannot-connect-to-aiml-files-in/26973015#26973015
+
+    String path = "D:/university courses/A.terms/Final project/program-ab-0.0.6.26/bots";
+    Bot bot = new Bot(botname, path);
+
+    Chat chatSession = new Chat(bot);
+    String request = "Hello. Are you alive? What is your name?";
+    String response = chatSession.multisentenceRespond(request);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView t = new TextView(this);
+        t = (TextView) findViewById(R.id.title_text);
+        t.setText(response);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,6 +69,7 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
+/*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -36,4 +84,43 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+*/
+
+/*
+    public class MainActivity extends Activity
+    {
+        String botname = "alice2";
+        String path = "D:/university courses/A.terms/Final project/program-ab-0.0.6.26/bots";
+        Bot bot = new Bot(botname, path);
+
+
+        Chat chatSession = new Chat(bot);
+        String request = "Hello. Are you alive? What is your name?";
+        String response = chatSession.multisentenceRespond(request);
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            TextView t = new TextView(this);
+            t = (TextView) findViewById(R.id.text);
+            t.setText(response);
+
+        }
+    }
+*/
+
+
+/*
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu)
+        {
+// Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+*/
+
+
+    }
